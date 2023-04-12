9f000447a095fb90d512450389fdcdabed3413db
mcflower <greenfild@gmail.com>
2023-04-12 21:33:54 +0400

    Update RuleSearch.php
    namespace

--- src/models/RuleSearch.php
+++ src/models/RuleSearch.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace amintado\rbacplus\models;
+namespace mcflower\rbacplus\models;
 
 use Yii;
 use yii\data\ArrayDataProvider;
 
 /**
- * @author John Martin <john.itvn@gmail.com>
+ * @author Ilya Zelenskiy <elias-green@yandex.ru>
  * @since 1.0.0
  */
 class RuleSearch extends Rule {
